14eaed05ae6bac4c7fe5e57459dc420741aef415
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2024-06-17 00:39:07 +0300

    Build

---
+++ src/app/shared/components/overlay/overlay.component.ts
@@ -0,0 +1,123 @@
+import { HttpClientModule } from '@angular/common/http';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { Overlay, OverlayModule, OverlayRef } from '@angular/cdk/overlay';
+import { CdkPortal, PortalModule } from '@angular/cdk/portal';
+import { OverlayService } from '../../services/overlay.service';
+@Component({
+  selector: 'app-overlay',
+  standalone: true,
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    OverlayModule,
+    PortalModule,
+  ],
+  templateUrl: './overlay.component.html',
+  styleUrl: './overlay.component.scss',
+})
+export class OverlayComponent {
+  @Input() connectedTo: any;
+  @Input() text!: string;
+  @Input() get id(): number {
+    return this._id;
+  }
+  set id(id: number) {
+    if (typeof id === 'string') {
+      this._id = parseInt(id);
+    } else {
+      this._id = id;
+    }
+  }
+  private _id!: number;
+  @Output() closed = new EventEmitter<any>();
+  @ViewChild(CdkPortal) portal!: ElementRef;
+  overlayRef!: OverlayRef;
+  private nativeElement: any;
+
+  constructor(
+    private overlay: Overlay,
+    private renderer: Renderer2,
+    private overlayService: OverlayService
+  ) {}
+
+  ngOnInit() {
+    this.overlayService.registerOverlay(this);
+    console.log(this.connectedTo);
+    if (this.connectedTo.getBoundingClientRect) {
+      this.nativeElement = this.connectedTo;
+    } else {
+      this.nativeElement = this.connectedTo._elementRef.nativeElement;
+    }
+  }
+
+  public showOverlay() {
+    const positionStrategy = this.overlay
+      .position()
+      .flexibleConnectedTo(this.nativeElement)
+      .withPositions([
+        {
+          originX: 'start',
+          originY: 'center',
+          overlayX: 'end',
+          overlayY: 'center',
+          offsetX: -10,
+        },
+        {
+          originX: 'end',
+          originY: 'center',
+          overlayX: 'start',
+          overlayY: 'center',
+          offsetX: 10,
+        },
+        {
+          originX: 'center',
+          originY: 'bottom',
+          overlayX: 'center',
+          overlayY: 'top',
+          offsetY: 10,
+        },
+      ])
+      .withGrowAfterOpen();
+    const scrollStrategy = this.overlay.scrollStrategies.reposition();
+    const overlayRef = this.overlay.create({
+      positionStrategy,
+      scrollStrategy,
+      hasBackdrop: true,
+      backdropClass: 'my-backdrop',
+    });
+    this.overlayRef = overlayRef;
+    overlayRef.detachments().subscribe(() => {
+      this.renderer.removeClass(this.nativeElement, 'elevate');
+      this.renderer.removeAttribute(this.nativeElement, 'id');
+    });
+    overlayRef.attach(this.portal);
+    this.renderer.addClass(this.nativeElement, 'elevate');
+    this.renderer.setAttribute(this.nativeElement, 'id', 'onboarding-active');
+    overlayRef.backdropClick().subscribe(() => this.hideOverlay());
+  }
+
+  public hideOverlay() {
+    if (this.overlayRef && this.overlayRef.hasAttached) {
+      this.overlayService.wasClosed(this._id);
+      this.overlayRef.dispose();
+      this.closed.emit();
+    }
+  }
+
+  ngOnDestroy() {
+    this.hideOverlay();
+    this.overlayService.destroyOverlay(this);
+  }
+}
